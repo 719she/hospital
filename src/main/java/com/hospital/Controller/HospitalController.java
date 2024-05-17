ea790452dc7675cff93bd2f89058e52971ee5428
rudvl <rudvlf9939@naver.com>
2024-05-18 01:57:34 +0900

    test commit (html add)

---
+++ src/main/java/com/hospital/Controller/HospitalController.java
@@ -0,0 +1,13 @@
+package com.hospital.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HospitalController {
+
+    @GetMapping("/")
+    public String home() {
+        return "hospital_home";
+    }
+}
