596215fd94ade8f28dedec84586fd26e69081478
rudvl <719she@gmail.com>
2024-05-18 02:47:04 +0900

    test

--- src/main/java/com/hospital/Controller/HospitalController.java
+++ src/main/java/com/hospital/Controller/HospitalController.java
@@ -1,7 +1,10 @@
 package com.hospital.Controller;
 
+import com.hospital.Dto.UserDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HospitalController {
@@ -9,5 +12,13 @@ public class HospitalController {
     @GetMapping("/")
     public String home() {
         return "hospital/h_home";
+    }
+
+    @PostMapping ("/")
+    public String login(UserDto userDto) {
+        if(userDto.getId()=="1111" && userDto.getPassword()=="1111") {
+            return "hospital/h_1111";
+        }
+        return "redirect:/";
     }
 }
